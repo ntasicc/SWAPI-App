82aea511d9794057c2a7c99785b149f420fbdec7
ntasicc <ntasicc@gmail.com>
2022-06-23 13:51:37 +0200

    Form validation

--- swapi-app/src/components/NewCharacter/NewCharacter.jsx
+++ swapi-app/src/components/NewCharacter/NewCharacter.jsx
@@ -3,6 +3,29 @@ import Modal from "../UI/Modal";
 import { swDataActions } from "../../store/swData-slice";
 import { Formik, Field, Form } from "formik";
 import classes from "./NewCharacter.module.css";
+import * as Yup from "yup";
+
+const DisplayingErrorMessagesSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  height: Yup.number("Need a number")
+    .typeError("Must be a number")
+    .required("Required")
+    .positive("Must be positive")
+    .integer("Must be integer"),
+  mass: Yup.number()
+    .typeError("Must be a number")
+    .required("Required")
+    .positive("Must be positive")
+    .integer("Must be integer"),
+  hair_color: Yup.string().required("Required"),
+  skin_color: Yup.string().required("Required"),
+  eye_color: Yup.string().required("Required"),
+  birth_year: Yup.string().min(4, "Too Short!").required("Required"),
+  gender: Yup.string().required("Required"),
+});
 
 const NewCharacter = (props) => {
   const dispatch = useDispatch();
@@ -25,21 +48,65 @@ const NewCharacter = (props) => {
           birth_year: "",
           gender: "",
         }}
+        validationSchema={DisplayingErrorMessagesSchema}
         onSubmit={(values) => addCharacterHandler(values)}
       >
-        <Form className={classes.form}>
-          <InputComponent name={"name"} text={"Name"} />
-          <InputComponent name={"height"} text={"Height"} />
-          <InputComponent name={"mass"} text={"Mass"} />
-          <InputComponent name={"hair_color"} text={"Hair Color"} />
-          <InputComponent name={"skin_color"} text={"Skin Color"} />
-          <InputComponent name={"eye_color"} text={"Eye Color"} />
-          <InputComponent name={"birth_year"} text={"Birth Year"} />
-          <InputComponent name={"gender"} text={"Gender"} />
-          <button className={classes.addBtn} type="submit">
-            Add
-          </button>
-        </Form>
+        {({ errors, touched }) => (
+          <Form className={classes.form}>
+            <InputComponent
+              name={"name"}
+              text={"Name"}
+              errors={errors.name}
+              touched={touched.name}
+            />
+            <InputComponent
+              name={"height"}
+              text={"Height"}
+              errors={errors.height}
+              touched={touched.height}
+            />
+            <InputComponent
+              name={"mass"}
+              text={"Mass"}
+              errors={errors.mass}
+              touched={touched.mass}
+            />
+            <InputComponent
+              name={"hair_color"}
+              text={"Hair Color"}
+              errors={errors.hair_color}
+              touched={touched.hair_color}
+            />
+            <InputComponent
+              name={"skin_color"}
+              text={"Skin Color"}
+              errors={errors.skin_color}
+              touched={touched.skin_color}
+            />
+            <InputComponent
+              name={"eye_color"}
+              text={"Eye Color"}
+              errors={errors.eye_color}
+              touched={touched.eye_color}
+            />
+            <InputComponent
+              name={"birth_year"}
+              text={"Birth Year"}
+              errors={errors.birth_year}
+              touched={touched.birth_year}
+            />
+            <InputComponent
+              name={"gender"}
+              text={"Gender"}
+              errors={errors.gender}
+              touched={touched.gender}
+            />
+
+            <button className={classes.addBtn} type="submit">
+              Add
+            </button>
+          </Form>
+        )}
       </Formik>
     </Modal>
   );
@@ -48,7 +115,12 @@ const NewCharacter = (props) => {
 const InputComponent = (props) => {
   return (
     <div className={classes.inputDiv}>
-      <label htmlFor={props.name}>{props.text}</label>
+      <label htmlFor={props.name}>
+        {props.text}{" "}
+        {props.touched && props.errors && (
+          <span className="text-red-700 text-sm"> ({props.errors})</span>
+        )}
+      </label>
       <Field id={props.name} name={props.name} />
     </div>
   );
